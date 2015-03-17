a6ed8c5a3aee9c6fdbf188d97243cde642d7f94b
Aaron Trout <aatrout@cisco.com>
2015-03-17 16:41:41 +0000

    Install ruby from RVM so we get a newer version

--- ubuntu_headless_install.sh
+++ ubuntu_headless_install.sh
@@ -7,8 +7,12 @@
 set -e
 
 apt-get update
-apt-get install -y ruby bundler
+apt-get install -y curl
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+\curl -sSL https://get.rvm.io | bash -s stable --ruby
+source /usr/local/rvm/scripts/rvm
 
+gem install bundler
 bundle install
 
 ln -s ${PWD}/vsphere_file_manager.rb /usr/local/bin/vsphere_file_manager
